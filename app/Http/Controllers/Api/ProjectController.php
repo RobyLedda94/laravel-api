77f98ce0d711f63dc217c023d375da313a0942e2
RobyLedda94 <robyledda01@yahoo.it>
2024-10-14 16:18:50 +0200

    [UPD] milestone 1 definito il controller e la rotta, restituisce in formato json i post

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::all();
+        return response()->json([
+            'success' => true,
+            'results' => $posts
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// importo il controller
+use App\Http\Controllers\Api\ProjectController as ProjectController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// creo la rotta
+Route::get('/posts',[ProjectController::class, 'index'])->name('posts');
