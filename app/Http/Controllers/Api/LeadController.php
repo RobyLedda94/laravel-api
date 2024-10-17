d0020016c5efe5cb2603aa3064ea4f323a42150b
RobyLedda94 <robyledda01@yahoo.it>
2024-10-17 17:02:12 +0200

    [UPD] definite regole di validazione ed errori

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -1,10 +1,34 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator; // importo validator
+use App\Models\Lead; // utilizzo il model
 
 class LeadController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        // Validazione dei dati in ingresso
+        $validator = Validator::make($data, [
+            'name' => ['required', 'max:100'],
+            'surname' => ['required', 'max:100'],
+            'email' => ['required', 'max:100'],
+            'contact' => ['required', 'max:100'],         
+        ],
+        $errors = [
+            'name.require' => 'Inserire il nome',
+            'surname.require' => 'Inserire il cognome',
+            'email.require' => 'Inserire email',
+            'contact.require' => 'Inserire il contatto',
+            'name.max' => 'Il nome non deve superare :max caratteri',
+            'surname.max' => 'Il cognome non deve superare :max caratteri',
+            'email.max' => 'L\' email non deve superare :max caratteri',
+            'contact.max' => 'Il contatto non deve superare :max caratteri',
+        ]);
+    }
 }
