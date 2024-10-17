476ed81e9f039a002fd1b6b09d53aa398a5a472d
RobyLedda94 <robyledda01@yahoo.it>
2024-10-17 16:24:18 +0200

    [UPD] step 3 creata tabella nel db

--- database/migrations/2024_10_17_141156_create_leads_table.php
+++ database/migrations/2024_10_17_141156_create_leads_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('leads', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('surname', 100);
+            $table->string('email', 100);
+            $table->string('contact', 100);
             $table->timestamps();
         });
     }
